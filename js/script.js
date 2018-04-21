40e8a6fa325829249ab6687d31906b849d9b8f25
Denis Schuele <schueledenis@gmx.at>
2018-04-21 05:36:55 +0200

    calc is missing

--- js/script.js
+++ js/script.js
@@ -3,3 +3,24 @@ function myFunction(){
 	// $$$$$$$$$$$$$$$$$--------------have to fix that the content move to the header while navbar is hidden
 	// document.getElementById("content").classList.toggle("margin-top-10");
 }
+var priceAmster = 1189.00;
+var priceLondon = 2360.00;
+// var tripA = Amsterdam;
+// var tripL = London;
+var priceAll = priceLondon + priceAmster;
+
+
+
+document.write()
+function bookLondon(){
+	document.getElementById("price-london").innerHTML = "nice";
+}
+// document.write()
+// function bookAmsterdam(){
+// 	document.write("navbar").classList.toggle("visible");
+// 	;
+// }
+// function book() {
+//     var myWindow = window.open("", "MsgWindow", "width=200,height=100");
+//     myWindow.document.write("<p>This is 'MsgWindow'. I am 200px wide and 100px tall!</p>");
+// }
