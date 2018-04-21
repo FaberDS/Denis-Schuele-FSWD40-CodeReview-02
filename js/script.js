ec3015b498618a13a7fba1906e1a0286cd36cca1
Denis Schuele <schueledenis@gmx.at>
2018-04-21 02:47:44 +0200

    index.html ready

---
+++ js/script.js
@@ -0,0 +1,5 @@
+function myFunction(){
+	document.getElementById("navbar").classList.toggle("visible");
+	// $$$$$$$$$$$$$$$$$--------------have to fix that the content move to the header while navbar is hidden
+	// document.getElementById("content").classList.toggle("margin-top-10");
+}
